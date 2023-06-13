2b9701e3d5659bb753a6e668280c0b3ac60e927f
ghaldunts_707 <ghaldunts_707@ghaldunts.local>
2023-06-13 12:40:11 +0400

    commit

--- script.js
+++ script.js
@@ -78,6 +78,23 @@ window.addEventListener("load", () => {
     const date = new Date(dateString);
     return date.toLocaleDateString("en-US", options);
   }
+
+  function setReminder(taskDiv, deadline) {
+    const now = new Date();
+    const taskDeadline = new Date(deadline);
+    const timeDiff = taskDeadline - now;
+
+    if (timeDiff > 0) {
+      setTimeout(() => {
+        alert(
+          `Reminder: Task deadline approaching for "${
+            taskDiv.querySelector(".text").value
+          }"!`
+        );
+      }, timeDiff);
+    }
+  }
+
   function updateTaskCount() {
     const completedTasks = document.querySelectorAll('.task input[readonly][style="text-decoration: line-through;"]').length;
     const pendingTasks = document.querySelectorAll('.task input[readonly]:not([style="text-decoration: line-through;"])').length;
@@ -85,19 +102,14 @@ window.addEventListener("load", () => {
     const notCompletedCountElement = document.getElementById('pending-count');
     completedCountElement.textContent = `Completed: ${completedTasks}`;
     notCompletedCountElement.textContent = `Pending: ${pendingTasks}`;
-    const now = new Date();
+
     const tasks = document.querySelectorAll(".task");
     tasks.forEach((task) => {
       const deadlineInput = task.querySelector(".deadline");
-      const deadline = new Date(deadlineInput.value);
-      if (deadline - now < 86400000) {
-        deadlineInput.classList.add("red");
-      } else {
-        deadlineInput.classList.remove("red");
-      }
-      if (deadline - now < 3600000) {
-        // Less than an hour remaining
-        alert("Task deadline approaching!");
+      const deadline = deadlineInput.value;
+      if (deadline) {
+        const taskDiv = task;
+        setReminder(taskDiv, deadline);
       }
     });
   }
